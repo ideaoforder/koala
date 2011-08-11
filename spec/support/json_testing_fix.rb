4787b32535a398c7e6d4c944044fb4ab21cf1319
Alex Koppel <alex+git@alexkoppel.com>
2011-08-11 14:57:40 +0200

    Added back debugging statements.

--- spec/support/json_testing_fix.rb
+++ spec/support/json_testing_fix.rb
@@ -10,9 +10,9 @@ module MultiJson
       # if it's a hash, recreate it with k/v pairs inserted in sorted-by-key order
       # (for some reason, REE 1.8.7 fails if we don't assign the ternary result as a local variable
       # separately from calling encode_original)
-      #puts "Object: #{object.inspect}"
+      puts "Object: #{object.inspect}"
       new_object = sort_object(object)
-      #puts "New object: #{new_object.inspect}"
+      puts "New object: #{new_object.inspect}"
       encode_original(new_object)
     end
 
