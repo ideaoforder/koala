8520c7f76b58fb5528e0902f64df96ca0391ea09
Alex Koppel <alex+git@alexkoppel.com>
2011-08-11 14:01:10 +0200

    RBX 2.0 JSON matching is failing on Travis for arrays with hashes in them; expanding encode_with_ordering to look at array contents.

--- spec/support/json_testing_fix.rb
+++ spec/support/json_testing_fix.rb
@@ -8,11 +8,23 @@ module MultiJson
   def encode_with_ordering(object)
     # if it's a hash, recreate it with k/v pairs inserted in sorted-by-key order
     # (for some reason, REE 1.8.7 fails if we don't assign the ternary result as a local variable
-    # separate from calling encode_original)
-    new_object = object.is_a?(Hash) ? object.keys.sort.inject({}) {|hash, k| hash[k] = object[k]; hash} : object
+    # separately from calling encode_original)
+    new_object = if object.is_a?(Hash)
+      sort_hash(object)
+    elsif object.is_a?(Array)
+      object.collect {|item| item.is_a?(Hash) ? sort_hash(item) : item}
+    else
+      object
+    end
     encode_original(new_object)
   end
 
   alias_method :encode_original, :encode
   alias_method :encode, :encode_with_ordering
+    
+  private 
+  
+  def sort_hash(object)
+    object.keys.sort.inject({}) {|hash, k| hash[k] = object[k]; hash}
+  end
 end
